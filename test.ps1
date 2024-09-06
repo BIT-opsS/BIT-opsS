31a305876cc17647d43c60fe51e496f6d1592d34
BIT-opsS <wino.anas@olx.co.id>
2024-09-06 18:23:33 +0700

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,13 @@
+# Membaca konten file
+$content = Get-Content -Path "C:\Windows\Temp\joko\_tmp.txt"
+$foundStrings = @()
+# Mencari string di dalam tanda kutip
+foreach ($line in $content) {
+    # Mencari semua string yang ada di dalam tanda kutip
+    $matches = [regex]::Matches($line, 'name="([^"]*)"')
+    foreach ($match in $matches) {
+        $value = $match.Groups[1].Value
+	#$foundStrings += $value	
+        Write-Host "String yang ditemukan:" $value"
+    }
+}
